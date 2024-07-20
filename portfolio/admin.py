f10dd44537a8f83828766f6cf31335fae88f54a0
muslimbek77 <muslimjonbekali@mail.ru>
2024-07-20 20:17:24 +0500

    customize contact admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Contact
+from django.utils.html import format_html
+
 # Register your models here.
 
-admin.site.register((Contact))
+
+@admin.register(Contact)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('name','email')
+    
